cfba3fcde864e0ef72cdfae2685df73b2f32411d
Devin Coleman <croilders48@gmail.com>
2024-10-20 21:43:53 -0700

    Add show and hidden to seaonal jokes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.querySelector('#randomJokeButton').addEventListener('click', joke)
+document.querySelector('#punchLine').addEventListener('click', show)
 
 
 const programmingCheckbox = document.getElementById("programming")
@@ -7,6 +8,7 @@ const miscCheckbox = document.getElementById("misc")
  const punCheckbox = document.getElementById("pun")
  const spookyCheckbox = document.getElementById("spooky")
  const christmasCheckbox = document.getElementById("christmas")
+ let h5 = document.getElementById('hey')
 let mySubmit = document.getElementById("submit")
 let url = "https://v2.jokeapi.dev/joke/Any?type=single"
 document.getElementById('submit2').addEventListener('submit', runz)
@@ -21,6 +23,7 @@ fetch(url)
 
     function runz(event){
    event.preventDefault()
+   h5.style.display = "none"
    if(christmasCheckbox.checked){
     url = "https://v2.jokeapi.dev/joke/Any?type=twopart"
     url = url.replace("Any", "Christmas")
@@ -28,12 +31,14 @@ fetch(url)
     url = url = "https://v2.jokeapi.dev/joke/Any?type=twopart"
     url = url.replace("Any", "Spooky")
 
+   }else {
+    url = "https://v2.jokeapi.dev/joke/Spooky,Christmas?type=twopart"
    }
-   console.log(url)
+   
    fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(data)
+
      document.querySelector('h3').innerHTML = data.setup
      document.querySelector('h5').innerHTML = data.delivery
    
@@ -42,6 +47,13 @@ fetch(url)
 
     }
 
+  function show(){
+    if(h5.style.display === "none"){
+      h5.style.display = "flex"
+    }else{
+      h5.style.display = "none"
+    }
+}
 
 
 
