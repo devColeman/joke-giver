b516d34b71929bf3e61ffff870a3f4fd2fb9c543
Devin Coleman <croilders48@gmail.com>
2024-10-20 17:57:59 -0700

    added some js functions

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 document.querySelector('#randomJokeButton').addEventListener('click', joke)
 
 
+const programmingCheckbox = document.getElementById("programming")
+mySubmit = document.getElementById("submit")
+let url = "https://v2.jokeapi.dev/joke/Any?type=single"
+
+mySubmit.onclick = function(event){
+    event.preventDefault()
+
+ if(programmingCheckbox.checked === true){
+   url = url.replace("Any", "Programming" )
+    
+} else{
+    url = "https://v2.jokeapi.dev/joke/Any?type=single"
+}
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+       console.log(data.joke)
+     document.querySelector('h2').innerHTML = data.joke
+   
+   } )
+console.log(url)
+
+}
+
+
+
+
+
+
+
 function joke(){
     fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single")
     .then(res => res.json())
